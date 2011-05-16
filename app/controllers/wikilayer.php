695b739e6681e12284db870cceebb43622ed3a5d
Jillian Ada Burrows <Jillian@RepresentationDesign.net>
2011-05-15 20:39:10 -0700

    attempting to use token to retrieve relationships of the me object on fb

--- app/controllers/wikilayer.php
+++ app/controllers/wikilayer.php
@@ -123,7 +123,7 @@ class wikilayer extends controller {
       );
 
       //$this->user_tokens->add($data);
-      $data = $this->fb->get_relationships('me');
+      $data = $this->fb->get_relationships('jillian.burrows');
     }
 
     return($data);
